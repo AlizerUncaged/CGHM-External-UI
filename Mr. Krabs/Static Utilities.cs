709f5bc355c68955ae676218a3f36b4bc790bf60
Alizer <floydguevarra58@gmail.com>
2021-11-03 00:12:08 +0800

    yes, ui stuff

---
+++ Mr. Krabs/Static Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mr.Krabs {
+    public static class Static_Utilities {
+
+        public static Random Random = new Random(DateTime.UtcNow.Second + DateTime.UtcNow.Millisecond);
+
+        public static double RandomDouble(double max, double min = 0) {
+            return Random.NextDouble() * (min - max) + max;
+        }
+
+    }
+}

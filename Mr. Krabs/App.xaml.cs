abf5dc510345ac98e09b135ec00856454774f5de
Alizer <saylent1@gmail.com>
2021-11-20 14:03:55 +0800

    📦 Clean App.cs exception catching.

--- Mr. Krabs/App.xaml.cs
+++ Mr. Krabs/App.xaml.cs
@@ -14,16 +14,31 @@ namespace Mr.Krabs {
     public partial class App : Application {
 
         protected override void OnStartup(StartupEventArgs e) {
+            // catch all exceptions if not on debug mode
+
+#if (!DEBUG) 
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-             base.OnStartup(e);
+#endif
+            base.OnStartup(e);
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            ExceptionCatched(e.Exception);
         }
 
         private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
-            MessageBox.Show(
-                $"Please screenshot this and paste on issue page. {Environment.NewLine}An unhandled error occured: {Environment.NewLine}{e.ExceptionObject.ToString()}{Environment.NewLine}Press OK to file an issue.",
-                @"Please screenshot this and paste on issue page.",
-                MessageBoxButton.OK, 
-                MessageBoxImage.Error);
+            if (e.ExceptionObject is Exception ex)
+                ExceptionCatched(ex);
+        }
+
+        private void ExceptionCatched(Exception ex) {
+            if (ex != null)
+                MessageBox.Show(
+                    $"Please screenshot this and paste on issue page. {Environment.NewLine}An unhandled error occured: {Environment.NewLine}{ex}{Environment.NewLine}Press OK to file an issue.",
+                    @"Please screenshot this and paste on issue page.",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
 
             const string issueLink = @"https://github.com/AlizerUncaged/CGHM-External-UI/issues/new";
             Process.Start(issueLink);

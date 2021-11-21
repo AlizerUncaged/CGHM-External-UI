cb6887aa4f6c4d5ddfbf31c32cf62bae1c99b1dd
Alizer <saylent1@gmail.com>
2021-11-21 13:48:39 +0800

    🔧 Changed default assembly settings.

--- Mr. Krabs/Properties/AssemblyInfo.cs
+++ Mr. Krabs/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mr. Krabs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CGHM External")]
+[assembly: AssemblyDescription("Mod for Crab Game.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mr. Krabs")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 1887")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.7.420")]
+[assembly: AssemblyFileVersion("1.0.7.69")]

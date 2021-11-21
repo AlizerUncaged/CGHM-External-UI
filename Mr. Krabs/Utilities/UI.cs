8bdbc60b30f6a21d017b941255352562aa46ca09
Alizer <saylent1@gmail.com>
2021-11-21 14:56:01 +0800

    🎨 🐎 Max framerate for animations set 60fps.

--- Mr. Krabs/Utilities/UI.cs
+++ Mr. Krabs/Utilities/UI.cs
@@ -10,6 +10,7 @@ namespace Mr.Krabs.Utilities {
     public static class UI {
         public static void RunAnimation(this FrameworkElement window, string resourceName) {
             Storyboard sb = window.FindResource(resourceName) as Storyboard;
+            Timeline.SetDesiredFrameRate(sb, 60);
             if (sb != null) { sb.Begin(); }
         }
 

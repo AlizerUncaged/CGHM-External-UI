57d035601f7941bbde28cb014b67e4f42bbec12c
Alizer <saylent1@gmail.com>
2021-11-22 09:18:06 +0800

    📦 Preparation for update.

---
+++ Mr. Krabs/Utilities/String Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mr.Krabs.Utilities {
+    public static class String_Utilities {
+        // null terminates a string
+        public static string NullTerminate(this string str) {
+            return str + '\0';
+        }
+    }
+}
